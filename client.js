9b2cb6b5999a6c843a195474a7f81462a63afb2b
Kevin Chu <kvnchu2@gmail.com>
2020-12-02 22:15:05 +0000

    experimented with move

--- client.js
+++ client.js
@@ -11,6 +11,8 @@ const connect = function() {
   conn.on('data', data => {console.log(data)});
   conn.on('connect', () => {console.log("Successfully connected to game server")})
   conn.on('connect', () => {conn.write('Name: KKC')})
+
+
   return conn;
 }
 
