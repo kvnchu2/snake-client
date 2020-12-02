62871a0497bd673a81ff5c4f19f383acf8937858
Kevin Chu <kvnchu2@gmail.com>
2020-12-02 21:26:19 +0000

    added in name and welcome message

--- client.js
+++ client.js
@@ -8,9 +8,10 @@ const connect = function() {
   });
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
-  conn.on('data',data => {console.log(data)});
-
+  conn.on('data', data => {console.log(data)});
+  conn.on('connect', () => {console.log("Successfully connected to game server")})
+  conn.on('connect', () => {conn.write('Name: KKC')})
   return conn;
 }
 
-module.exports = { connect};
+module.exports = { connect };
