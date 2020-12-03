fe20d6f5d7581aedc3b21cbf4b3b07e8e1cd9c8f
Kevin Chu <kvnchu2@gmail.com>
2020-12-03 02:34:17 +0000

    added in constants and exported ip and host

--- client.js
+++ client.js
@@ -1,11 +1,12 @@
 const net = require('net');
+const { IP, PORT } = require('./constants')
 
 
 const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
+  const conn = net.createConnection({
+    host: IP,
+    port: PORT
+  })
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
   conn.on('data', data => {console.log(data)});
